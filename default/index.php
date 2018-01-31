30862324cf5f524f1a1ef750d409eb51517eae27
Jean <demeljean@hotmail.fr>
2018-01-31 12:27:37 +0100

    added subfolder namespace example

--- default/index.php
+++ default/index.php
@@ -3,5 +3,10 @@ spl_autoload_register();
 
 use zoo\Cat;
 
+use zoo\herbivorous\Cow;
+
 $cat = new Cat("Garfeild", "siamois", ["orange","black"]);
 var_dump($cat);
+
+
+var_dump(new Cow());
---
+++ default/zoo/herbivorous/cow.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace zoo\herbivorous;
+
+use zoo\Animal;
+
+class Cow extends Animal {
+
+}
