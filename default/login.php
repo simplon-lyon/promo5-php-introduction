38b36a589361fe4d61295ca296845873906be2eb
matmorel <matmorel@users.noreply.github.com>
2018-02-06 17:02:27 +0100

    Update login.php

--- default/login.php
+++ default/login.php
@@ -2,15 +2,19 @@
 
 $email1 = "bloup@bloup.com";
 $password1 = "mdp";
-
+// on vérifie que le mail et le mot de passe sont bien transmis dans le POST
 if(isset($_POST["email"]) && isset($_POST["password"])){
+  // ... puis on vérifie qu'ils sont valide
   if($_POST["email"]=== $email1 && $_POST["password"]===$password1){
+    // si c'est ok, on affiche "on a gagné"
     echo "on a gagné";
   }
   else{
+    //si non, on affiche "on a perdu"
     echo "on a perdu";
   }
 } else {
+  // si le POST est vide, on affiche "bien essayé"
   echo "bien essayé";
 }
 
