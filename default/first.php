62a1df4a7893669cd06327a98f4a05414da33d1d
matmorel <matmorel@users.noreply.github.com>
2018-02-06 16:23:35 +0100

    Update first.php

--- default/first.php
+++ default/first.php
@@ -1,7 +1,7 @@
 <?php
-
+// on créé un tableau contenant les jours de la semaine
 $week = array("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
-
+// on créé un tableau à deux dimensions (des tableaux dans un tableau) qui représente le menu de la semaine
 $list = [
   ["lundi", "patates", "glace"],
   ["mardi", "riz", "mousse au chocolat"],
@@ -23,6 +23,7 @@ $list = [
 <body>
   <ul>
     <?php
+      // on affiche chaque jours de la semaine dans des <li>
       for ($i = 0; $i < count($week); $i++) {
         echo("<li>{$week[$i]}</li>");
       }
@@ -30,12 +31,15 @@ $list = [
   </ul>
   <table>
      <?php
+        // on affiche le menu dans une table html
         for ( $i = 0; $i < count ($list); $i++){
+          // pour chaque élément paire du tableau, on ajoute l'attribut style background-color: green
           if($i % 2){
             echo("<tr style=\"background-color:green;\">");
           }else{
             echo("<tr>");
           }
+          // on affiche chaque élément du menu dans un <td>
           for ($j=0; $j < count($list[$i]); $j++) {
             echo("<td>{$list[$i][$j]}</td>");
           }
